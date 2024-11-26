15921deb484e0fbe22cee96762e6c8e1c7b7b45c
Deokyeol Park <qlgod777777@gmail.com>
2024-11-27 00:43:29 +0900

    Added the function to extract URL addresses of players for data collection and collect entries for players' non-missing stats.

--- collect_data.py
+++ collect_data.py
@@ -10,10 +10,9 @@ import time
 import csv
 
 mainUrl = "https://www.fotmob.com"
+wd = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 def get_team_url(result):
-    wd = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    
     wd.get(mainUrl + "/ko/leagues/47/overview/premier-league")
     time.sleep(1)
     
@@ -45,7 +44,10 @@ def get_player_url(GK, DF, MF, FW, team_url):
             elif i == 4:
                 for code in playerCodes:
                     FW.append(code['href'])
+    return
 
+
+def make_url_csv(GK, DF, MF, FW):
     with open('url/gk_url.csv', mode='w', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
         for code in GK:
@@ -65,22 +67,21 @@ def get_player_url(GK, DF, MF, FW, team_url):
     return
 
 
-def stat_cnt(result, codes):
+def get_meaningful_data(result, codes, real_codes):
     for code in codes:
-        wd = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
         wd.get(mainUrl + code)
+        time.sleep(1)
         
         try:
             # 버튼 클릭 대기
-            button = wd.find_element(By.CLASS_NAME, 'css-a7bq51-FilterButton.e1aie6871')
+            button = wd.find_element(By.CLASS_NAME, 'css-a7bq51-FilterButton.e1tb2kvp1')
             button.click()
-            
-            # 페이지 로드 대기
-            time.sleep(1)
             
             # 페이지 소스 가져오기
             soupPlayer = BeautifulSoup(wd.page_source, 'html.parser')
-            stats = soupPlayer.select("div.css-2duihq-StatTitle.e1uibvo11")
+            stats = soupPlayer.select("div.css-17js6f6-PlayerPageGridCSS.e17ysukt0 > div.css-14y4cbw-Column-LeftColumnCSS.e17ysukt1 > div.css-1wb2t24-CardCSS.e1mlfzv61")
+            stats = stats[3]
+            stats = stats.select("div.css-2duihq-StatTitle.e1uibvo11")
             
             for stat in stats:
                 if stat.string in result:
@@ -88,52 +89,64 @@ def stat_cnt(result, codes):
                 else:
                     result[stat.string] = 1
             
+            if len(stats) != 0:
+                real_codes.append(code)
+            
         except Exception as e:
-            print()
-    
-    print(result)
+            pass
     return
 
 
-def get_player_info(result, codes):
-    for code in codes:
-        wd = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-        wd.get(mainUrl + code)
-        
-        try:
-            # 버튼 클릭 대기
-            button = wd.find_element(By.CLASS_NAME, 'css-a7bq51-FilterButton.e1aie6871')
-            button.click()
-            
-            # 페이지 로드 대기
-            time.sleep(1)
-            
-            # 페이지 소스 가져오기
-            soupPlayer = BeautifulSoup(wd.page_source, 'html.parser')
-            stats = soupPlayer.select("div.css-jb6lgd-StatValue.e1uibvo12 > span") 
-            
-            temp = []
-            for stat in stats:
-                temp.append(stat.string)
-            result.append(temp)
-        
-        except Exception as e:
-            print()
-        
-    wd.quit()
-            
+def make_evaluation_items_csv(GK, DF, MF, FW):
+    gktemp = []; dftemp = []; mftemp = []; fwtemp = []
+    for key, value in GK.items():
+        if value == GK['경고']:
+            gktemp.append(key)
+    for key, value in DF.items():
+        if value == DF['경고']:
+            dftemp.append(key)
+    for key, value in MF.items():
+        if value == MF['경고']:
+            mftemp.append(key)
+    for key, value in FW.items():
+        if value == FW['경고']:
+            fwtemp.append(key)
+    with open('evaluation items/gk.csv', mode='w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        for item in gktemp:
+            writer.writerow([item])
+    with open('evaluation items/df.csv', mode='w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        for item in dftemp:
+            writer.writerow([item])
+    with open('evaluation items/mf.csv', mode='w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        for item in mftemp:
+            writer.writerow([item])
+    with open('evaluation items/fw.csv', mode='w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        for item in fwtemp:
+            writer.writerow([item])
     return
 
 
 def main():
     teamCode = []
+    TempGKCodes = []; TempDFCodes= []; TempMFCodes = []; TempFWCodes = []
     GKCodes = []; DFCodes= []; MFCodes = []; FWCodes = []
     playerStats = []
     GKStatDic = {}; DFStatDic = {}; MFStatDic = {}; FWStatDic = {}
     
     get_team_url(teamCode)
-    get_player_url(GKCodes, DFCodes, MFCodes, FWCodes, teamCode)
+    get_player_url(TempGKCodes, TempDFCodes, TempMFCodes, TempFWCodes, teamCode)
     
-    # stat_cnt(GKStatDic, GKCodes)
+    get_meaningful_data(GKStatDic, TempGKCodes, GKCodes)
+    get_meaningful_data(DFStatDic, TempDFCodes, DFCodes)
+    get_meaningful_data(MFStatDic, TempMFCodes, MFCodes)
+    get_meaningful_data(FWStatDic, TempFWCodes, FWCodes)
+    
+    make_url_csv(GKCodes, DFCodes, MFCodes, FWCodes)
+    make_evaluation_items_csv(GKStatDic, DFStatDic, MFStatDic, FWStatDic)
+
 if __name__ == '__main__':
     main()
